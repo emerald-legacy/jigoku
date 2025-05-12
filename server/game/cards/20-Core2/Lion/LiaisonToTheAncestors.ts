1b5bb4fcc0e12a41f5483a670a0c2c5d71291c0d
Luiz Socrate <luiz@socrate.com.br>
2025-05-12 21:19:55 +0200

    Fix: Liaison to the Ancestors

--- server/game/cards/20-Core2/Lion/LiaisonToTheAncestors.ts
+++ server/game/cards/20-Core2/Lion/LiaisonToTheAncestors.ts
@@ -1,24 +1,24 @@
-import AbilityDsl from '../../../abilitydsl';
 import { CardTypes } from '../../../Constants';
+import AbilityDsl from '../../../abilitydsl';
 import DrawCard from '../../../drawcard';
 
 export default class LiaisonToTheAncestors extends DrawCard {
-    static id = 'liaison-to-the-ancestors';
+  static id = 'liaison-to-the-ancestors';
 
-    setupCardAbilities() {
-        this.wouldInterrupt({
-            title: 'Protect the honor of a character',
-            when: {
-                onCardDishonored: (event: { card: DrawCard }, context) =>
-                    event.card.type === CardTypes.Character &&
-                    event.card.controller === context.player &&
-                    (context.player.dynastyDiscardPile as Array<DrawCard>).some(
-                        (card) => event.card.printedCost < card.printedCost
-                    )
-            },
-            gameAction: AbilityDsl.actions.honor((context: any) => ({
-                target: context.event.card
-            }))
-        });
-    }
+  setupCardAbilities() {
+    this.reaction({
+      title: 'Protect the honor of a character',
+      when: {
+        onCardDishonored: (event: { card: DrawCard }, context) =>
+          event.card.type === CardTypes.Character &&
+          event.card.controller === context.player &&
+          (context.player.dynastyDiscardPile as Array<DrawCard>).some(
+            (card) => event.card.printedCost < card.printedCost,
+          ),
+      },
+      gameAction: AbilityDsl.actions.honor((context: any) => ({
+        target: context.event.card,
+      })),
+    });
+  }
 }
