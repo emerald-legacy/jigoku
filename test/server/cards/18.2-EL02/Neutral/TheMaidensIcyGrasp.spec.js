e0422240a5f8297b783aa854ba5e2cf29d4cf5ca
Luiz Socrate <luiz@socrate.com.br>
2025-06-11 23:41:18 +0200

    test The Maiden's Icy Grasp

--- test/server/cards/18.2-EL02/Neutral/TheMaidensIcyGrasp.spec.js
+++ test/server/cards/18.2-EL02/Neutral/TheMaidensIcyGrasp.spec.js
@@ -5,24 +5,28 @@ describe('The Maidens Icy Grasp', function () {
                 phase: 'conflict',
                 player1: {
                     inPlay: ['miya-mystic'],
-                    hand: ['the-maiden-s-icy-grasp']
+                    hand: ['the-maiden-s-icy-grasp','village-doshin']
                 },
                 player2: {
                     honor: 11,
-                    inPlay: ['daidoji-uji', 'kitsu-spiritcaller'],
-                    hand: ['tattooed-wanderer', 'shosuro-miyako-2'],
+                    inPlay: ['daidoji-uji', 'kitsu-spiritcaller','shiba-ryuu'],
+                    hand: ['tattooed-wanderer', 'shosuro-miyako-2','one-with-the-sea','the-maiden-s-icy-grasp'],
                     dynastyDiscard: ['doji-challenger']
                 }
             });
             this.grasp = this.player1.findCardByName('the-maiden-s-icy-grasp');
             this.mystic = this.player1.findCardByName('miya-mystic');
+            this.villageDoshin = this.player1.findCardByName('village-doshin');
 
             this.miyako = this.player2.findCardByName('shosuro-miyako-2');
             this.uji = this.player2.findCardByName('daidoji-uji');
+            this.uji.honor();
             this.spiritcaller = this.player2.findCardByName('kitsu-spiritcaller');
             this.wanderer = this.player2.findCardByName('tattooed-wanderer');
             this.challenger = this.player2.findCardByName('doji-challenger');
-            this.uji.honor();
+            this.ryuu = this.player2.findCardByName('shiba-ryuu');
+            this.oneWithTheSea = this.player2.findCardByName('one-with-the-sea');
+            this.grasp2 = this.player2.findCardByName('the-maiden-s-icy-grasp');
 
             this.noMoreActions();
             this.initiateConflict({
@@ -31,6 +35,9 @@ describe('The Maidens Icy Grasp', function () {
                 defenders: [this.uji, this.spiritcaller]
             });
         });
+
+        describe('base cases',function () {
+
 
         it('character from hand', function () {
             this.player2.clickCard(this.wanderer);
@@ -74,5 +81,36 @@ describe('The Maidens Icy Grasp', function () {
             );
             expect(this.getChatLogs(2)).toContain('Military Air conflict - Attacker: 1 Defender: 8');
         });
+
+        it('also works when conflict has alternative', function () {
+            this.player2.clickCard(this.oneWithTheSea);
+            this.player2.clickCard(this.ryuu);
+
+            this.player1.clickCard(this.villageDoshin);
+            this.player1.clickPrompt('0');
+            this.player1.clickPrompt('Conflict');
+
+            this.player2.clickCard(this.grasp2);
+            expect(this.player2).toHavePrompt('Choose a character');
+            this.player2.clickCard(this.villageDoshin);
+            expect(this.getChatLogs(5)).toContain(
+                "player2 plays The Maiden's Icy Grasp to prevent Village Dōshin from contributing to resolution of this conflict"
+            );
+            expect(this.getChatLogs(10)).toContain(`Military Air conflict - Attacker: ${
+            this.mystic.getMilitarySkill()
+            +this.mystic.getPoliticalSkill()
+            } Defender: ${
+            this.uji.getMilitarySkill()
+            +this.uji.getPoliticalSkill()
+            +this.spiritcaller.getMilitarySkill()
+            +this.spiritcaller.getPoliticalSkill()
+            +this.ryuu.getMilitarySkill()
+            +this.ryuu.getPoliticalSkill()
+            }`);
+
+            this.noMoreActions();
+            expect(this.getChatLogs(2)).toContain('player2 won a military conflict 21 vs 2');
+        });
     });
 });
+})
