4e4e5a02f70f0587dfc551345d8baa933183141c
Benjamin Wolf <benjamin.wolf.de@me.com>
2026-05-07 21:07:33 +0200

    Set facdown ID properly

--- server/game/cards/AshigaruRecruit.ts
+++ server/game/cards/AshigaruRecruit.ts
@@ -31,10 +31,11 @@ export class AshigaruRecruit extends DrawCard {
 
     getSummary(activePlayer: Player, hideWhenFaceup: boolean) {
         const summary = super.getSummary(activePlayer, hideWhenFaceup);
-        const tokenProps =
-            activePlayer === this.controller
-                ? { id: this.facedownCard.cardData.id as string, isToken: true }
-                : { isToken: true };
+        const tokenProps: Record<string, unknown> = { isToken: true };
+        if(activePlayer === this.controller) {
+            tokenProps.facedownId = this.facedownCard.cardData.id;
+            tokenProps.facedownPackId = this.facedownCard.packId;
+        }
         return Object.assign(summary, tokenProps);
     }
 }
