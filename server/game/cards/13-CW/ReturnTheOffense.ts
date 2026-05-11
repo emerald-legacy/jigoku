6bb167ccb3741280a616a6ef85c919e924bc9ee2
Benjamin Wolf <benjamin.wolf.de@me.com>
2026-05-11 23:47:51 +0200

    Add check for participation

--- server/game/cards/13-CW/ReturnTheOffense.ts
+++ server/game/cards/13-CW/ReturnTheOffense.ts
@@ -1,6 +1,6 @@
 import DrawCard from '../../drawcard';
 import AbilityDsl from '../../abilitydsl';
-import { DuelTypes } from '../../Constants';
+import { CardTypes, DuelTypes, Players } from '../../Constants';
 
 class ReturnTheOffense extends DrawCard {
     static id = 'return-the-offense';
@@ -8,29 +8,47 @@ class ReturnTheOffense extends DrawCard {
     setupCardAbilities() {
         this.action({
             title: 'Initiate a political duel',
-            initiateDuel: {
-                type: DuelTypes.Political,
-                message: '{0}{1}{2}{3}{4}',
-                messageArgs: duel => [
-                    duel.winner,
-                    duel.winner ? ' does not bow as a result of conflict resolution' : '',
-                    duel.loser ? ' and ' : '',
-                    duel.loser,
-                    duel.loser ? ' cannot be readied' : ''
-                ],
-                gameAction: duel => AbilityDsl.actions.multiple([
-                    AbilityDsl.actions.cardLastingEffect(({
-                        target: duel.winner,
-                        effect: AbilityDsl.effects.doesNotBow()
-                    })),
-                    AbilityDsl.actions.cardLastingEffect({
-                        target: duel.loser,
-                        effect: AbilityDsl.effects.cardCannot({
-                            cannot: 'ready',
-                            restricts: 'cardEffects'
-                        })
-                    })
-                ])
+            condition: () => this.game.isDuringConflict(),
+            targets: {
+                challenger: {
+                    cardType: CardTypes.Character,
+                    controller: Players.Any,
+                    cardCondition: (card) => card.isParticipating()
+                },
+                duelTarget: {
+                    dependsOn: 'challenger',
+                    cardType: CardTypes.Character,
+                    controller: Players.Any,
+                    cardCondition: (card, context) =>
+                        card.isParticipating() &&
+                        card !== context.targets.challenger &&
+                        card.controller !== context.targets.challenger.controller,
+                    gameAction: AbilityDsl.actions.duel((context) => ({
+                        type: DuelTypes.Political,
+                        challenger: context.targets.challenger,
+                        message: '{0}{1}{2}{3}{4}',
+                        messageArgs: duel => [
+                            duel.winner,
+                            duel.winner ? ' does not bow as a result of conflict resolution' : '',
+                            duel.loser ? ' and ' : '',
+                            duel.loser,
+                            duel.loser ? ' cannot be readied' : ''
+                        ],
+                        gameAction: duel => AbilityDsl.actions.multiple([
+                            AbilityDsl.actions.cardLastingEffect({
+                                target: duel.winner,
+                                effect: AbilityDsl.effects.doesNotBow()
+                            }),
+                            AbilityDsl.actions.cardLastingEffect({
+                                target: duel.loser,
+                                effect: AbilityDsl.effects.cardCannot({
+                                    cannot: 'ready',
+                                    restricts: 'cardEffects'
+                                })
+                            })
+                        ])
+                    }))
+                }
             }
         });
     }
