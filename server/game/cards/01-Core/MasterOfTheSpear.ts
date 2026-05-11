e4f063453c8ccf77c98f28e8a2d0cd27ed81b85d
Benjamin Wolf <benjamin.wolf.de@me.com>
2026-05-11 21:33:51 +0200

    Add check for participating character

--- server/game/cards/01-Core/MasterOfTheSpear.ts
+++ server/game/cards/01-Core/MasterOfTheSpear.ts
@@ -1,5 +1,5 @@
 import DrawCard from '../../drawcard';
-import { Players } from '../../Constants';
+import { Players, CardTypes } from '../../Constants';
 
 class MasterOfTheSpear extends DrawCard {
     static id = 'master-of-the-spear';
@@ -11,7 +11,9 @@ class MasterOfTheSpear extends DrawCard {
             target: {
                 player: Players.Opponent,
                 activePromptTitle: 'Choose a character to send home',
+                cardType: CardTypes.Character,
                 controller: Players.Opponent,
+                cardCondition: card => card.isParticipating(),
                 gameAction: ability.actions.sendHome()
             }
         });
