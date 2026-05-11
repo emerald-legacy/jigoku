79097b8f2925d5b45e2bfb2e19783dc83d94c754
Benjamin Wolf <benjamin.wolf.de@me.com>
2026-05-12 00:01:17 +0200

    Remove redundant test

--- test/server/cards/13-CW/HallowedGround.spec.js
+++ test/server/cards/13-CW/HallowedGround.spec.js
@@ -123,55 +123,5 @@ describe('Hallowed Ground', function() {
                 expect(this.player2.honor).toBe(this.startingHonor - 4);
             });
         });
-
-        describe('Hallowed Grounds\' earth ring ability', function() {
-            beforeEach(function() {
-                this.setupTest({
-                    phase: 'conflict',
-                    player1: {
-                        inPlay: ['border-rider'],
-                        dynastyDiscard: ['hallowed-ground']
-                    },
-                    player2: {
-                        inPlay: ['border-rider', 'hida-yakamo']
-                    }
-                });
-                this.player1.placeCardInProvince('hallowed-ground', 'province 1');
-                this.attacker = this.player2.findCardByName('border-rider');
-                this.attacker2 = this.player2.findCardByName('hida-yakamo');
-                this.noMoreActions();
-                this.player1.passConflict();
-                this.noMoreActions();
-            });
-
-            it('should not prevent ring claim if opponent does not have the earth ring', function() {
-                this.initiateConflict({
-                    ring: 'fire',
-                    type: 'military',
-                    attackers: [this.attacker, this.attacker2],
-                    defenders: []
-                });
-                this.noMoreActions();
-                // Province broke; player2 is prompted to discard dynasty card
-                this.player2.clickPrompt('No');
-                expect(this.getChatLogs(10)).not.toContain('player2 cannot claim rings this conflict due to the constant effect of Hallowed Ground');
-            });
-
-            it('should prevent ring claim when the opponent has the earth ring and breaks a province', function() {
-                this.player2.claimRing('earth');
-                this.game.checkGameState(true);
-                this.initiateConflict({
-                    ring: 'fire',
-                    type: 'military',
-                    attackers: [this.attacker, this.attacker2],
-                    defenders: []
-                });
-                this.noMoreActions();
-                // After conflict action window passes, the province breaks. Player2 is prompted to discard the dynasty card.
-                this.player2.clickPrompt('No');
-                expect(this.getChatLogs(10)).toContain('player2 cannot claim rings this conflict due to the constant effect of Hallowed Ground');
-                expect(this.game.rings.fire.claimed).toBe(false); // fire ring not claimed due to restriction
-            });
-        });
     });
 });
