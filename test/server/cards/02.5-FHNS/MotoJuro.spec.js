1864cb88ddf33b478b6f90ace72d75f9ea07c5bf
Benjamin Wolf <benjamin.wolf.de@me.com>
2026-05-12 18:00:30 +0200

    Fix test cases

--- test/server/cards/02.5-FHNS/MotoJuro.spec.js
+++ test/server/cards/02.5-FHNS/MotoJuro.spec.js
@@ -20,40 +20,28 @@ describe('Moto Juro', function() {
                 });
             });
 
-            it('should offer a choice to move into conflict when at home', function() {
+            it('should move Moto Juro into the conflict when at home', function() {
                 this.player2.pass();
                 this.player1.clickCard(this.motoJuro);
-                expect(this.player1).toHavePrompt('Moto Juro');
-                expect(this.player1.currentButtons).toContain('Move into conflict');
-            });
-
-            it('should move Moto Juro into the conflict', function() {
-                this.player2.pass();
-                this.player1.clickCard(this.motoJuro);
-                this.player1.clickPrompt('Move into conflict');
                 expect(this.motoJuro.isParticipating()).toBe(true);
             });
 
             it('should move Moto Juro home when already participating', function() {
                 this.player2.pass();
                 this.player1.clickCard(this.motoJuro);
-                this.player1.clickPrompt('Move into conflict');
                 this.player2.pass();
                 this.player1.clickCard(this.motoJuro);
-                this.player1.clickPrompt('Move home');
                 expect(this.motoJuro.isParticipating()).toBe(false);
             });
 
             it('should be limited to twice per round', function() {
                 this.player2.pass();
                 this.player1.clickCard(this.motoJuro);
-                this.player1.clickPrompt('Move into conflict');
                 this.player2.pass();
                 this.player1.clickCard(this.motoJuro);
-                this.player1.clickPrompt('Move home');
                 this.player2.pass();
                 this.player1.clickCard(this.motoJuro);
-                expect(this.player1).not.toHavePrompt('Choose an option');
+                expect(this.player1).toHavePrompt('Conflict Action Window');
             });
         });
     });
