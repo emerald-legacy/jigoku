67844eca6bd7f3c820416fb992935f0c316e4d91
Benjamin Wolf <benjamin.wolf.de@me.com>
2026-09-24 10:33:23 +0200

    Beautify statement

--- server/game/cards/09.3-JfS/IntoTheForbiddenCity.ts
+++ server/game/cards/09.3-JfS/IntoTheForbiddenCity.ts
@@ -10,7 +10,7 @@ export default class IntoTheForbiddenCity extends ProvinceCard {
             title: 'Discard an attachment',
             target: {
                 cardType: CardType.Attachment,
-                cardCondition: (card) => card.parentCharacter?.isAttacking() === true,
+                cardCondition: (card) => !!card.parentCharacter?.isAttacking(),
                 gameAction: AbilityDsl.actions.discardFromPlay()
             }
         });
